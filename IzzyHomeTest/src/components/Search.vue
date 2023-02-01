2ac675aa9874c6fce2851c6cbddf20c333d8cd9a
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-02-01 11:19:06 +0100

    1.2 filter

--- IzzyHomeTest/src/components/Search.vue
+++ IzzyHomeTest/src/components/Search.vue
@@ -1,33 +1,39 @@
 <script>
     export default {
         name: 'Search',
-    
     }
 </script>
 
 <template>
-    <form action="/" method="get">
+    <form action="" method="get"> 
         <div>
-            <label for="is_furnished">Meublé | Nom meublé</label>
-            <input type="radio" name="is_furnished">
-            <input type="radio" name="is_furnised">
+            <label for="is_furnished">Meublé</label>
+            <input type="radio" name="is_furnished" value="furnished" >
+            <input type="radio" name="is_furnised" value="not_furnished" >
         </div>
+    
         <div>
             <label for="type">Maison | Appartement</label>
-            <input type="radio" name="type">
-            <input type="radio" name="type">
+            <input type="radio" name="type" >
+            <input type="radio" name="type" >
         </div>
         <div>
             <label for="living_area">Surface habitanle (en m²)</label>
-            <input type="number" name="min_living_area" placeholder="Min">
-            <input type="number" name="max_living_area" placeholder="Max">
+            <input type="number" name="min_living_area" placeholder="Min" >
+            <input type="number" name="max_living_area" placeholder="Max" >
         </div>        
         <div>
             <label for="full_rent_price">Loyer</label>
-            <input type="number" name="min_full_rent_price" placeholder="Min">
-            <input type="number" name="max_lfull_rent_price" placeholder="Max">
+            
+            <input type="number" name="min_full_rent_price" placeholder="Min" >
+            <input type="number" name="max_full_rent_price" placeholder="Max" >
         </div>
-    </form>
+        <div>
+            <button type="submit" name="Search">Rechercher</button>
+            <button type="reset">Reset</button>
+        </div>
+
+   </form>
 </template>
 
 
