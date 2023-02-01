b95dd2e0612f85725b9670340ef5e635aa816cdc
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-02-01 02:14:34 +0100

    0.2 transmission of data to the card

--- IzzyHomeTest/src/components/Card.vue
+++ IzzyHomeTest/src/components/Card.vue
@@ -1,42 +1,70 @@
 <script>
-import bdd from '../data.js';
 export default {
-    setup(){
-        console.log(bdd);
-    },
-    data(){
-        return {
-            bdd,
+    props: {
+        item : {
+            type: Object,
+            default: () => ({})
         }
-    }
+    },
 }
 </script>
 
 
 <template>
-  <section>
     <figure>
-        <img :src="bdd[0]['main_picture']" alt="">
+        <img :src="item['main_picture']" :alt="item['title']">
+        <img src="../assets/Rectangle 1.png" alt="" class="filter">
     </figure>
+    
+    <p class="card--title">{{item['title']}} </p>
     <div class="container--flex">
         <div class="items--left">
-            <p>{{bdd[0]['title']}}</p>
-            <p>{{bdd[0]['living_area'] }} m²</p>
+            <p>Surface : {{item['living_area'] }} m²</p>
+            <p v-if="item['type']==flat">Appartement à {{item['address'] }} </p>
+            <p v-else>Maison à {{item['address']['city']}} ( {{item['address']['postal_code']}} )</p>
         </div>
         <div class="items--right">
-            <p>{{ bdd[0]['full_rent_price'] }} €</p>
+            <p>  {{ item['full_rent_price'] }} €</p>
+            
         </div>
         
     </div>
-  </section>
 </template>
 
 <style lang="scss" scoped>
-    section {
-        width: 35%;
+
+    figure{
+        position: relative;
+        img{
+            width: 100%;
+        }
     }
-    img{
-        width: 100%;
+    .filter{
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        
     }
+    .card--title{
+        position: absolute;
+        top: 20px;
+        width: 250px;
+        left: calc(50% - 125px);
+        text-align: center;
+    }
+    
+    .items--left{
+
+    }
+    .items--right{
+        margin-left: 20px;
+        p{
+            width: max-content;
+            background-color: #3a4cbd;
+            border-radius: 50px;
+            padding: 2px 10px;
+        }
+    }
+
 
 </style>
--- IzzyHomeTest/src/components/RowCard.vue
+++ IzzyHomeTest/src/components/RowCard.vue
@@ -1,23 +1,30 @@
 <script>
 import Card from '../components/Card.vue';
+import bdd from '../data.js'
 export default {
   name:'RowCard',
   components: {
     Card,
-
   },
-  // setup(){
-  //   console.log(data);
-  //   console.log(data[0]['title']);
-  // }
+  data(){
+    return {
+        bdd,
+    }
+  },
 }
 </script>
 
 <template>
     <div class="container--flex row">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <section v-for="item in bdd" :key="item.id">
+            <Card :item="item"/>
+        </section>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    section {
+        width: 35%;
+        min-width: 300px;
+    }
+</style>
